db75b840903de6d37cbc963d7367f7109430d299
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-15 21:43:27 -0500

    create new workout

--- controllers/api/workout.js
+++ controllers/api/workout.js
@@ -4,15 +4,16 @@ var db = require("../../models");
 // get all workouts from the API, route => ('api/workouts')
 router.get("/", (req, res) => {
 	db.Workout.find({})
-		.then((dbWorkout) => {
-			console.log(dbWorkout);
-			res.json(dbWorkout);
-			console.log(dbWorkout);
-		})
-		.catch((err) => {
-			console.log(err);
-			res.json(err);
-		});
+		.then((workouts) => res.json(workouts))
+		.catch((err) => res.json(err));
+});
+
+// create new workout
+router.post("/", (req, res) => {
+	// inserting a new workout
+	db.Workout.create(req.body) // create new workout
+		.then((workout) => res.json(workout)) // send success
+		.catch((err) => res.json(err)); // send error
 });
 
 module.exports = router;
