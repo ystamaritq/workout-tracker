7742440f7a31deea11093f60be5d7cc9b42f6071
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-15 22:55:21 -0500

    add exercise by the workout id

--- controllers/api/workout.js
+++ controllers/api/workout.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 var db = require("../../models");
+const mongoose = require("mongoose");
 
 // get all workouts from the API, route => ('api/workouts')
 router.get("/", (req, res) => {
@@ -14,6 +15,33 @@ router.post("/", (req, res) => {
 	db.Workout.create(req.body) // create new workout
 		.then((workout) => res.json(workout)) // send success
 		.catch((err) => res.json(err)); // send error
+});
+
+// add exercise by the workout id
+router.put("/:id", (req, res) => {
+	db.Exercise.create(req.body) // create a new exercise
+		.then(
+			(
+				{ _id } // get the new id
+			) =>
+				db.Workout.findOneAndUpdate(
+					// update workout
+					{ _id: mongoose.Types.ObjectId(req.params.id) }, // where id is given id
+					{ $push: { exercises: _id } }, // add reference to new exercise to workout array
+					{ new: true } // return the updated object
+				).then((workout) => res.json(workout)) // send success
+		)
+		.catch((err) => {
+			console.log(err);
+			res.json(err);
+		}); // send error;;
+});
+
+//
+router.get("/range", (req, res) => {
+	db.Workout.find({})
+		.then((workouts) => res.json(workouts))
+		.catch((err) => res.json(err));
 });
 
 module.exports = router;
