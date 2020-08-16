a5ab309d0a064c8dd57a6232f4851c3d51ca1f29
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-15 23:30:50 -0500

    moved exercise inside workout doc

--- controllers/api/workout.js
+++ controllers/api/workout.js
@@ -19,22 +19,14 @@ router.post("/", (req, res) => {
 
 // add exercise by the workout id
 router.put("/:id", (req, res) => {
-	db.Exercise.create(req.body) // create a new exercise
-		.then(
-			(
-				{ _id } // get the new id
-			) =>
-				db.Workout.findOneAndUpdate(
-					// update workout
-					{ _id: mongoose.Types.ObjectId(req.params.id) }, // where id is given id
-					{ $push: { exercises: _id } }, // add reference to new exercise to workout array
-					{ new: true } // return the updated object
-				).then((workout) => res.json(workout)) // send success
-		)
-		.catch((err) => {
-			console.log(err);
-			res.json(err);
-		}); // send error;;
+	db.Workout.findOneAndUpdate(
+		// update workout
+		{ _id: mongoose.Types.ObjectId(req.params.id) }, // where id is given id
+		{ $push: { exercises: req.body } }, // add new exercise to workout array
+		{ new: true } // return the updated object
+	)
+		.then((workout) => res.json(workout)) // send success
+		.catch((err) => res.json(err)); // send error;;
 });
 
 //
